384c7e6858eb37897d6f977b2836ff62b81571a0
abdullahahadi <abdullah.ahadi78@gmail.com>
2026-08-29 12:23:25 +0430

    add: implement API endpoint to retrieve word definitions from CSV

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import pandas as pd
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<word>")
-def api(word):
-    
-    return {
-        "definition": word.capitalize(),
-        "word": word
-    }
 
-app.run()
+@app.route("/api/v1/<word>")
+def api(word:str):
+    try:
+        
+        df = pd.read_csv("dictionary.csv")
+        definition = df.loc[df["word"] == word]["definition"].iloc[0]
+
+        result_dictionary = {
+            "word": word,
+            "definition": definition,
+            }
+        return jsonify(result_dictionary)
+
+    except IndexError as e:
+        return jsonify({
+            "status":"error",
+            "message" :f"word {word} not found"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
